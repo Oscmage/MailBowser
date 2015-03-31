d34e3ab97adb6296a299cd80eba07a5badbb7871
Filip Hallqvist <hej@filiphallqvist.se>
2015-03-31 14:28:02 +0200

    Removed old project

--- MailBowser/src/main/java/edu/chl/MailBowser/model/Project.java
+++
@@ -1,5 +0,0 @@
-package edu.chl.MailBowser.model;
-
-public class Project {
-	public static final String PROJECT_WINDOW_TEXT = "MailBowser";
-}
--- MailBowser/src/test/java/edu/chl/MailBowser/model/ProjectTest.java
+++
@@ -1,4 +0,0 @@
-package edu.chl.MailBowser.model;
-
-public class ProjectTest {
-}
