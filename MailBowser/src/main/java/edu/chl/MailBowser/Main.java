46e9cef51eff984b3d101de02dcbeaa61492165e
Mats Högberg <mats@hgbrg.se>
2015-03-31 15:01:46 +0200

    Updated package struture

--- MailBowser/src/main/java/edu/chl/MailBowser/Main.java
+++ MailBowser/src/main/java/edu/chl/MailBowser/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package edu.chl.MailBowser;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("MailBowser.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
