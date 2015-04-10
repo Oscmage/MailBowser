74eeb2970b2887e7455a9b68a38e45a064ab7190
Oscar Evertsson <oscar.evertsson@live.com>
2015-04-10 15:31:28 +0200

    Created a Email interface.

--- MailBowser/src/main/java/edu/chl/MailBowser/models/IEmail.java
+++ MailBowser/src/main/java/edu/chl/MailBowser/models/IEmail.java
@@ -1,8 +1,10 @@
 package edu.chl.MailBowser.models;
 
+import javax.mail.Session;
+
 /**
  * Created by OscarEvertsson on 10/04/15.
  */
 public interface IEmail {
-    javax.mail.Message getJavaxMessage();
+    javax.mail.Message getJavaxMessage(Session session);
 }
