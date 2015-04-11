5d7145b94079bb345b86f856df9924eb1336a9c2
Mats Högberg <mats@hgbrg.se>
2015-04-11 19:21:52 +0200

    Changed a few EmailAddresses to Address, and updated the documentation for both Address and IAddress

--- MailBowser/src/main/java/edu/chl/MailBowser/models/Address.java
+++ MailBowser/src/main/java/edu/chl/MailBowser/models/Address.java
@@ -5,34 +5,38 @@ import javax.mail.internet.InternetAddress;
 
 /**
  * Created by OscarEvertsson on 07/04/15.
- * EmailAddress represents an EmailAddress and have the possibility to check whether the address is correct or not.
+ *
+ * Address represents an email address. You cannot create an invalid Address obejct.
  */
-public class Address extends AbstractModel implements IAddress{
+public class Address extends AbstractModel implements IAddress {
     private InternetAddress address;
 
     /**
-     * Creates an EmailAddress object with the specified address trimmed(removes any space before and after the string).
+     * Creates an Address object with the specified String. The String is trimmed and validated upon creation.
      * If the address is invalid the position of where the address possibly could be wrong is printed out.
-     * @param address specifies the Email-address.
+     *
+     * @param address specifies the email address.
      */
-    public Address(String address){
+    public Address(String address) {
         try {
             this.address = new InternetAddress(address.trim());
         } catch (AddressException e) {
-            System.out.println(e.getPos()); //SUPPOSED TO DO SOMETHING LATER WHEN VIEW IS AVAILABLE (remove this when fixed).
+            // TODO: do something here other than just printing out the error
+            System.out.println(e.getPos());
         }
     }
 
-    /** Creates a new EmailAddress from an existing EmailAddress
+    /**
+     * Creates a new Address from an existing Address
      *
-     * @param emailAddress the address
+     * @param address the address to copy
      */
-    public Address(Address emailAddress) {
-        this(emailAddress.toString());
+    public Address(Address address) {
+        this(address.toString());
     }
 
     /**
-     * @return Gives a copy of the javax.mail.InternetAddress object which holds the information
+     * @return a copy of the javax.mail.InternetAddress object which holds the information
      */
     public InternetAddress getJavaxAddress() {
         return (InternetAddress)this.address.clone();
@@ -40,10 +44,11 @@ public class Address extends AbstractModel implements IAddress{
 
 
     /**
-     * Returns a string for the address.
-     * @return returns a string of the address.
+     * Returns a string representation of the address, in the format user@domain.com
+     *
+     * @return a string representation of the address
      */
-    public String toString(){
+    public String toString() {
         return this.address.toString();
     }
 }
