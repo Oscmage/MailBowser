cfb9e9cbd46a61886bb3878c791842721f353774
Jaxing <jesper_jaxing@hotmail.com>
2015-04-11 14:39:00 +0200

    Javadoc updated and parameters added

--- MailBowser/src/main/java/edu/chl/MailBowser/MailServer.java
+++ MailBowser/src/main/java/edu/chl/MailBowser/MailServer.java
@@ -4,5 +4,9 @@ package edu.chl.MailBowser;
  * Created by filip on 09/04/15.
  */
 public abstract class MailServer {
-    public abstract boolean send();
+    /**
+     * This method sends the email through the outgoing server of the account.
+     * @return the result of the method, true if send was successful else false
+     */
+    public abstract boolean send(Email email, Account account);
 }
