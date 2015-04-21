3f1f44fceb7c68d8523838c28f33451ab08c67c8
Jaxing <jesper_jaxing@hotmail.com>
2015-04-21 15:59:20 +0200

    MailServer no longer implements IMailServer

--- MailBowser/src/main/java/edu/chl/MailBowser/models/MailServer.java
+++ MailBowser/src/main/java/edu/chl/MailBowser/models/MailServer.java
@@ -5,7 +5,7 @@ import java.util.Properties;
 /**
  * Created by filip on 09/04/15.
  */
-public abstract class MailServer implements IMailServer{
+public abstract class MailServer{
     private String hostname;
     private String port;
 }
