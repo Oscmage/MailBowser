c940a58789a1dba5076660497c6d716968c6bccd
Jaxing <jesper_jaxing@hotmail.com>
2015-04-21 15:55:22 +0200

    Added JavaDoc

--- MailBowser/src/main/java/edu/chl/MailBowser/models/IncomingServer.java
+++ MailBowser/src/main/java/edu/chl/MailBowser/models/IncomingServer.java
@@ -4,10 +4,15 @@ import java.util.List;
 
 /**
  * Created by jesper on 2015-04-21.
+ * A concrete inplmentation of IIncomingServer.
  */
 public class IncomingServer extends MailServer implements IIncomingServer {
 
     @Override
+    /**
+     * fetches the emails from th server given in MailServer
+     *
+     */
     public List<Email> fetch(String username, String Password) {
         return null;
     }
