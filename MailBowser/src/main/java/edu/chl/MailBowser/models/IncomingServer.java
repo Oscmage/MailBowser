064baacfb3b3c53da9145dcf95e0e8365387b327
Mats Högberg <mats@hgbrg.se>
2015-04-24 15:26:29 +0200

    The fetch method in IncomingServer now fetches all mail from the INBOX folder and prints their subjects

--- MailBowser/src/main/java/edu/chl/MailBowser/models/IncomingServer.java
+++ MailBowser/src/main/java/edu/chl/MailBowser/models/IncomingServer.java
@@ -4,10 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.util.List;
 import java.util.Properties;
-import javax.mail.Session;
-import javax.mail.Store;
-import javax.mail.Folder;
-import javax.mail.Message;
+import javax.mail.*;
 
 /**
  * Created by jesper on 2015-04-21.
@@ -26,38 +23,42 @@ public class IncomingServer extends MailServer implements IIncomingServer {
         super(hostname, port);
     }
 
-    @Override
     /**
-     * fetches the emails from the server given in MailServer
+     * Fetches all emails from the server, using the supplied username and password.
      *
+     * @param username the username to authenticate with
+     * @param password the password to authenticate with
      */
-    public List<IEmail> fetch(String username, String Password) {
+    @Override
+    public List<IEmail> fetch(String username, String password) {
         Properties props = new Properties();
+        props.setProperty("mail.store.protocol", "imaps");
+
+        Session session = Session.getInstance(props, null);
+
         try {
-            props.load(new FileInputStream(new File("C:\\smtp.properties")));// Not sure if a file is created or if we have to create a new one with info
-            Session session = Session.getDefaultInstance(props, null);
+            Store store = session.getStore();
+            store.connect(getHostname(), username, password);
 
-            Store store = session.getStore("imaps");
-            store.connect("smtp.gmail.com", "mailbows3r@gmail.com","VG5!qBY&#f$QCmV");
+            Folder root = store.getFolder("INBOX");
+            root.open(Folder.READ_ONLY);
 
-            Folder allMails = store.getDefaultFolder();
-            allMails.open(Folder.READ_WRITE);
-            int messageCount = allMails.getMessageCount();
+            Message[] messages = root.getMessages();
 
-            System.out.println("Total Messages:- " + messageCount);
-
-            Message[] messages = allMails.getMessages();
-            System.out.println("------------------------------");
-            for (int i = 0; i < 10; i++) {
-                System.out.println("Mail Subject:- " + messages[i].getSubject());
+            for (int i = 0; i < messages.length; i++) {
+                try {
+                    System.out.println(messages[i].getSubject());
+                } catch (MessagingException e) {
+                    e.printStackTrace();
+                }
             }
-            allMails.close(true);
-            store.close();
 
-        } catch (Exception e) {
+            root.close(true);
+            store.close();
+        } catch (MessagingException e) {
             e.printStackTrace();
         }
+
         return null;
     }
-
 }
