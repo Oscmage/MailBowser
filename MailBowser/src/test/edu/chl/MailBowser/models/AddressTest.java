172af59535ce32d90cac069abbb20203114b8419
Oscar Evertsson <oscar.evertsson@live.com>
2015-04-30 10:08:49 +0200

    Created folder for testing.

---
+++ MailBowser/src/test/edu/chl/MailBowser/models/AddressTest.java
@@ -0,0 +1,18 @@
+package edu.chl.MailBowser.models;
+
+import org.junit.Test;
+/**
+ * Created by OscarEvertsson on 29/04/15.
+ */
+public class AddressTest {
+
+    @Test
+    public void testGetJavaxAddress() throws Exception {
+
+    }
+
+    @Test
+    public void testToString() throws Exception {
+
+    }
+}
