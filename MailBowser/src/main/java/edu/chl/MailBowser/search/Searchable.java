4ba8809eddfb58bba6709e7623e020bdac5cfbff
Mats Högberg <mats@hgbrg.se>
2015-05-05 22:04:04 +0200

    Added an interface for classes that should be searchable, and implemented a method that searches through sets

---
+++ MailBowser/src/main/java/edu/chl/MailBowser/search/Searchable.java
@@ -0,0 +1,17 @@
+package edu.chl.mailbowser.search;
+
+/**
+ * Created by mats on 05/05/15.
+ *
+ * An interface that enables searching of the objects that implements it.
+ */
+public interface Searchable {
+    /**
+     * Checks whether or not this object matches the query. It is up to each individual implementation
+     * to decide how to do the matching.
+     *
+     * @param query the query to match against
+     * @return true if the query matches the object, otherwise false
+     */
+    boolean matches(String query);
+}
