fbb2cd71645032dc3aff541c61bd769877a52288
Jaxing <jesper_jaxing@hotmail.com>
2015-05-05 14:56:49 +0200

    initial hash and equals methods written

--- MailBowser/src/main/java/edu/chl/MailBowser/models/Account.java
+++ MailBowser/src/main/java/edu/chl/MailBowser/models/Account.java
@@ -146,4 +146,33 @@ public class Account implements IAccount {
     public void fetch() {
         this.emails = incomingServer.fetch(getUsername(), password);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        if (!(o instanceof Account)) return false;
+
+        Account account = (Account) o;
+
+        if (address != null ? !address.equals(account.address) : account.address != null) return false;
+        if (emails != null ? !emails.equals(account.emails) : account.emails != null) return false;
+        if (incomingServer != null ? !incomingServer.equals(account.incomingServer) : account.incomingServer != null)
+            return false;
+        if (outgoingServer != null ? !outgoingServer.equals(account.outgoingServer) : account.outgoingServer != null)
+            return false;
+        if (password != null ? !password.equals(account.password) : account.password != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = address != null ? address.hashCode() : 0;
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (incomingServer != null ? incomingServer.hashCode() : 0);
+        result = 31 * result + (outgoingServer != null ? outgoingServer.hashCode() : 0);
+        result = 31 * result + (emails != null ? emails.hashCode() : 0);
+        return result;
+    }
 }
