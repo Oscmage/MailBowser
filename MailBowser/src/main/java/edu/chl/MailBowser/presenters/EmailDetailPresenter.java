282a86d1178d70b6b535e285e484ce75f7236690
Oscar Evertsson <oscar.evertsson@live.com>
2015-05-06 16:02:42 +0200

    Now implements IObserver.

--- MailBowser/src/main/java/edu/chl/MailBowser/presenters/EmailDetailPresenter.java
+++ MailBowser/src/main/java/edu/chl/MailBowser/presenters/EmailDetailPresenter.java
@@ -1,9 +1,27 @@
 package edu.chl.mailbowser.presenters;
 
+import edu.chl.mailbowser.email.models.Email;
+import edu.chl.mailbowser.email.models.IEmail;
+import edu.chl.mailbowser.event.EventBus;
+import edu.chl.mailbowser.event.EventType;
+import edu.chl.mailbowser.event.IEvent;
+import edu.chl.mailbowser.event.IObserver;
+
 /**
  * Created by filip on 04/05/15.
  */
-public class EmailDetailPresenter {
+public class EmailDetailPresenter implements IObserver{
 
+    public EmailDetailPresenter() {
+        EventBus.INSTANCE.register(this);
+    }
+
+    @Override
+    public void onEvent(IEvent evt) {
+        if(evt.getType() == EventType.SELECTED_EMAIL) {
+            IEmail email = (Email)evt.getValue();
+            
+        }
+    }
 }
 
