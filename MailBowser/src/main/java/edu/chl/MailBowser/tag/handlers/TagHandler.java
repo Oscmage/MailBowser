3a29bba727a892538eceb8409ce2c962dc11e292
Oscar Evertsson <oscar.evertsson@live.com>
2015-05-06 18:27:30 +0200

    TagHandler is now a singleton enum.

--- MailBowser/src/main/java/edu/chl/MailBowser/tag/handlers/TagHandler.java
+++ MailBowser/src/main/java/edu/chl/MailBowser/tag/handlers/TagHandler.java
@@ -14,21 +14,11 @@ import java.util.Set;
 /**
  * Created by OscarEvertsson on 29/04/15.
  */
-public class TagHandler{
-    private static TagHandler instance = new TagHandler();
+public enum TagHandler{
+    INSTANCE;
+    
     private Map<ITag,Set<IEmail>> tags = new HashMap<>();
     private Map<IEmail,Set<ITag>> emails = new HashMap<>();
-
-    private TagHandler(){}
-
-
-    /**
-     * Returns the instance of the singleton.
-     * @return
-     */
-    public static TagHandler getInstance(){
-        return instance;
-    }
 
     /**
      * Adds tag to an email.
