eff01beedca15905dd261f0336703a6a820cfa79
Mats Högberg <mats@hgbrg.se>
2015-05-07 16:14:09 +0200

    The fetch method in IncomingServer is now asynchronous

--- MailBowser/src/main/java/edu/chl/mailbowser/account/models/IncomingServer.java
+++ MailBowser/src/main/java/edu/chl/mailbowser/account/models/IncomingServer.java
@@ -2,6 +2,9 @@ package edu.chl.mailbowser.account.models;
 
 import edu.chl.mailbowser.email.models.Email;
 import edu.chl.mailbowser.email.models.IEmail;
+import edu.chl.mailbowser.event.Event;
+import edu.chl.mailbowser.event.EventBus;
+import edu.chl.mailbowser.event.EventType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +17,8 @@ import javax.mail.*;
  * A concrete implementation of IIncomingServer.
  */
 public class IncomingServer extends MailServer implements IIncomingServer {
+
+    private Fetcher fetcher = null;
 
     /**
      * Creates a new IncomingServer with the specified hostname and port.
@@ -32,48 +37,81 @@ public class IncomingServer extends MailServer implements IIncomingServer {
      * @param password the password to authenticate with
      */
     @Override
-    public List<IEmail> fetch(String username, String password) {
-        List<IEmail> emails = new ArrayList<>();
-
-        Properties props = new Properties();
-        props.setProperty("mail.store.protocol", "imaps");
-
-        Session session = Session.getInstance(props, null);
-
-        try {
-            Store store = session.getStore();
-            store.connect(getHostname(), username, password);
-
-            // start by getting the default (root) folder, and recursively work through all subfolders
-            Folder root = store.getDefaultFolder();
-            emails = recursiveFetch(root);
-
-            store.close();
-        } catch (MessagingException e) {
-            e.printStackTrace();
+    public void fetch(String username, String password) {
+        if (fetcher == null) {
+            fetcher = new Fetcher(username, password, new FetchCallback());
+            new Thread(fetcher).start();
         }
-
-        return emails;
     }
 
-    private List<IEmail> recursiveFetch (Folder folder) throws MessagingException {
-        List<IEmail> emails = new ArrayList<>();
+    private class FetchCallback implements Callback<List<IEmail>> {
 
-        if ((folder.getType() & Folder.HOLDS_MESSAGES) == Folder.HOLDS_MESSAGES) {
-            folder.open(Folder.READ_ONLY);
-            Message [] messages = folder.getMessages();
-            for (Message message : messages) {
-                emails.add(new Email(message));
-            }
+        @Override
+        public void onSuccess(List<IEmail> object) {
+            EventBus.INSTANCE.publish(new Event(EventType.FETCH_EMAILS, object));
+            fetcher = null;
         }
 
-        if ((folder.getType() & Folder.HOLDS_FOLDERS) == Folder.HOLDS_FOLDERS){
-            Folder [] folders = folder.list();
-            for (Folder subFolder : folders){
-                emails.addAll(recursiveFetch(subFolder));
-            }
+        @Override
+        public void onFailure(String msg) {
+            EventBus.INSTANCE.publish(new Event(EventType.FETCH_EMAILS_FAIL, null));
+            fetcher = null;
+        }
+    }
+
+    private class Fetcher implements Runnable {
+        private Callback<List<IEmail>> callback;
+        private String username;
+        private String password;
+
+        public Fetcher(String username, String password, Callback<List<IEmail>> callback) {
+            this.callback = callback;
         }
 
-        return emails;
+        @Override
+        public void run() {
+            List<IEmail> emails = new ArrayList<>();
+
+            Properties props = new Properties();
+            props.setProperty("mail.store.protocol", "imaps");
+
+            Session session = Session.getInstance(props, null);
+
+            try {
+                Store store = session.getStore();
+                store.connect(getHostname(), username, password);
+
+                // start by getting the default (root) folder, and recursively work through all subfolders
+                Folder root = store.getDefaultFolder();
+                emails = recursiveFetch(root);
+
+                store.close();
+            } catch (MessagingException e) {
+                callback.onFailure("Failed to fetch email from server");
+            }
+
+            callback.onSuccess(emails);
+        }
+
+        private List<IEmail> recursiveFetch (Folder folder) throws MessagingException {
+            List<IEmail> emails = new ArrayList<>();
+
+            if ((folder.getType() & Folder.HOLDS_MESSAGES) == Folder.HOLDS_MESSAGES) {
+                folder.open(Folder.READ_ONLY);
+                Message [] messages = folder.getMessages();
+                for (Message message : messages) {
+                    emails.add(new Email(message));
+                }
+            }
+
+            if ((folder.getType() & Folder.HOLDS_FOLDERS) == Folder.HOLDS_FOLDERS){
+                Folder [] folders = folder.list();
+                for (Folder subFolder : folders){
+                    emails.addAll(recursiveFetch(subFolder));
+                }
+            }
+
+            return emails;
+        }
     }
 }
