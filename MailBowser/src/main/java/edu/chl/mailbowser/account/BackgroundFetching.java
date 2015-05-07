03356a27686c81d874da516a22584d5d19376d21
Jaxing <jesper_jaxing@hotmail.com>
2015-05-07 15:55:44 +0200

    Created a thread to fetch email

---
+++ MailBowser/src/main/java/edu/chl/mailbowser/account/BackgroundFetching.java
@@ -0,0 +1,30 @@
+package edu.chl.mailbowser.account;
+
+import edu.chl.mailbowser.account.models.Account;
+
+/**
+ * Created by jesper on 2015-05-07.
+ */
+public class BackgroundFetching extends Thread {
+    private static BackgroundFetching instance = new BackgroundFetching();
+
+    private Account account = Account.getInstance();
+
+    private BackgroundFetching(){}
+
+    public static BackgroundFetching getInstance(){
+        return instance;
+    }
+
+    @Override
+    public void run() {
+        while (!this.isInterrupted()) {
+            account.fetch();
+            try {
+                this.sleep(30000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
