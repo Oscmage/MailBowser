e51f36023797cdb971e19c50fbf5bba7d5f39fd5
Jaxing <jesper_jaxing@hotmail.com>
2015-05-11 14:09:23 +0200

    IEmail implements Serializable

--- MailBowser/src/main/java/edu/chl/mailbowser/email/models/IEmail.java
+++ MailBowser/src/main/java/edu/chl/mailbowser/email/models/IEmail.java
@@ -3,13 +3,14 @@ package edu.chl.mailbowser.email.models;
 import edu.chl.mailbowser.search.Searchable;
 
 import javax.mail.Session;
+import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 
 /**
  * Created by OscarEvertsson on 10/04/15.
  */
-public interface IEmail extends Searchable {
+public interface IEmail extends Searchable, Serializable {
     javax.mail.Message getJavaxMessage(Session session);
     boolean isSent();
     IAddress getSender();
