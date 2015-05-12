0d8b233bb45c0c02b657b1b4b88801ae40f5ed6a
Jaxing <jesper_jaxing@hotmail.com>
2015-05-12 10:31:58 +0200

    Test for account created

--- MailBowser/src/test/java/edu/chl/mailbowser/account/models/AccountTest.java
+++ MailBowser/src/test/java/edu/chl/mailbowser/account/models/AccountTest.java
@@ -1,35 +1,34 @@
 package edu.chl.mailbowser.account.models;
 
-import edu.chl.mailbowser.email.models.Address;
+import edu.chl.mailbowser.email.models.Email;
+import edu.chl.mailbowser.email.models.IAddress;
+import edu.chl.mailbowser.email.models.IEmail;
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
+import java.util.ArrayList;
+import java.util.List;
+
+
 import static org.junit.Assert.assertTrue;
 
 public class AccountTest {
-    Account a = new Account(new Address("Jax@gmail.com"), "hej", new IncomingServer("@","@"), new OutgoingServer("@","@"));
-    Account b = new Account(new Address("Jax@gmail.com"), "hej", new IncomingServer("@","@"), new OutgoingServer("@","@"));
-    Account c = new Account(new Address("lol@lol.de"), "hej", new IncomingServer(" "," "), new OutgoingServer(" "," "));
+
 
     @Test
-    public void testEquals() throws Exception {
-        assertTrue(a.equals(a));
-        assertFalse(a.equals(null));
-        assertFalse(a.equals(new Object()));
-        assertFalse(a.equals(c));
-        assertTrue(a.equals(b));
+    public void testSend() throws Exception {
+        IOutgoingServer outgoingServer = new MockOutServer();
+        Account account = Account.INSTANCE;
+        account.send(new Email(new ArrayList<IAddress>(),"",""));
+        assertTrue(((MockOutServer)outgoingServer).called);
+
     }
 
     @Test
-    public void testHash() throws Exception {
-        assertTrue(a.hashCode() == a.hashCode());
-        assertTrue(a.hashCode() == b.hashCode());
-        assertFalse(a.hashCode() == c.hashCode());
-    }
+    public void testFetch() throws Exception {
+        IIncomingServer incomingServer = new MockInServer();
+        Account account = Account.INSTANCE;
+        account.fetch();
+        assertTrue(((MockInServer) incomingServer).called);
 
-    @Test
-    public void testEqualsAndHash() throws Exception {
-        assertTrue(!(a.equals(a)) || (a.hashCode() == a.hashCode()));
-        assertFalse(!(a.equals(a))||(a.hashCode() == c.hashCode()));
     }
 }
