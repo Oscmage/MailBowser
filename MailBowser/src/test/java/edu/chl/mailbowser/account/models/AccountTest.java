484b205d41f3943432e1b04dd515dad6a024ea12
Jaxing <jesper_jaxing@hotmail.com>
2015-05-12 11:12:57 +0200

    updated test to work on master

--- MailBowser/src/test/java/edu/chl/mailbowser/account/models/AccountTest.java
+++ MailBowser/src/test/java/edu/chl/mailbowser/account/models/AccountTest.java
@@ -1,8 +1,10 @@
 package edu.chl.mailbowser.account.models;
 
+import edu.chl.mailbowser.email.models.Address;
 import edu.chl.mailbowser.email.models.Email;
 import edu.chl.mailbowser.email.models.IAddress;
 import edu.chl.mailbowser.email.models.IEmail;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -12,23 +14,29 @@ import java.util.List;
 import static org.junit.Assert.assertTrue;
 
 public class AccountTest {
+    Account account;
+    MockInServer incomingServer;
+    MockOutServer outgoingServer;
+    MockEmail email;
 
+    @Before
+    public void init(){
+        incomingServer = new MockInServer();
+        outgoingServer = new MockOutServer();
+        email = new MockEmail();
+        account = new Account(new MockAddress(),"",incomingServer,outgoingServer);
+    }
 
     @Test
     public void testSend() throws Exception {
-        IOutgoingServer outgoingServer = new MockOutServer();
-        Account account = Account.INSTANCE;
-        account.send(new Email(new ArrayList<IAddress>(),"",""));
-        assertTrue(((MockOutServer)outgoingServer).called);
-
+        account.send(email);
+        assertTrue(outgoingServer.called);
+        assertTrue(email.setSender);
     }
 
     @Test
     public void testFetch() throws Exception {
-        IIncomingServer incomingServer = new MockInServer();
-        Account account = Account.INSTANCE;
         account.fetch();
-        assertTrue(((MockInServer) incomingServer).called);
-
+        assertTrue(incomingServer.called);
     }
 }
