f6e0b36031b3d9329c1c40eec98e5a1f72a41eba
Oscar Evertsson <oscar.evertsson@live.com>
2015-05-20 21:21:19 +0200

    Implemented equals and hashCode.

--- MailBowser/src/main/java/edu/chl/mailbowser/contact/Contact.java
+++ MailBowser/src/main/java/edu/chl/mailbowser/contact/Contact.java
@@ -62,4 +62,29 @@ public class Contact implements IContact {
     public boolean matches(String query) {
         return getFullName().matches(query);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        } else if (o == null) {
+            return false;
+        } else if (!o.getClass().equals(this.getClass())) {
+            return false;
+        }
+        Contact c = (Contact) o;
+        if (!this.firstName.equals(c.firstName)) {
+            return false;
+        } else if (!this.lastName.equals(c.getLastName())) {
+            return false;
+        } else if (!this.getEmailAddresses().equals(c.getEmailAddresses())) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode(){
+        return (firstName.hashCode() * 7)  + (lastName.hashCode() * 17 )+ (emailAddresses.hashCode() * 23 );
+    }
 }
