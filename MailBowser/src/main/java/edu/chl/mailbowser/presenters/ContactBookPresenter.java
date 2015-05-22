960db3770e8e2e22a7491fb2dfdfdab1ef54fcb0
Jaxing <jesper_jaxing@hotmail.com>
2015-05-22 14:00:12 +0200

    worked some on contactview

--- MailBowser/src/main/java/edu/chl/mailbowser/presenters/ContactBookPresenter.java
+++ MailBowser/src/main/java/edu/chl/mailbowser/presenters/ContactBookPresenter.java
@@ -3,10 +3,15 @@ package edu.chl.mailbowser.presenters;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
+
+import javax.swing.text.html.ListView;
+
 /**
  * Created by jesper on 2015-05-22.
  */
 public class ContactBookPresenter {
+    @FXML private ListView contactList;
+
     @FXML
     public void addContactButtonOnAction(ActionEvent actionEvent) {
 
