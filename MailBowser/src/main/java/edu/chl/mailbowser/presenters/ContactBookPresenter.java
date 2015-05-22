6f0a55bccc3c81aee858d163a9f26940ef8edb35
Jaxing <jesper_jaxing@hotmail.com>
2015-05-22 13:35:31 +0200

    First draft of contactbookview created

---
+++ MailBowser/src/main/java/edu/chl/mailbowser/presenters/ContactBookPresenter.java
@@ -0,0 +1,14 @@
+package edu.chl.mailbowser.presenters;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+/**
+ * Created by jesper on 2015-05-22.
+ */
+public class ContactBookPresenter {
+    @FXML
+    public void addContactButtonOnAction(ActionEvent actionEvent) {
+
+    }
+}
