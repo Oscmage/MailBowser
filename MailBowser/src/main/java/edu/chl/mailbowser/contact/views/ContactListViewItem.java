1a1e8e63d3bdb2724f9dbf2c2944e123f0ec0faf
Jaxing <jesper_jaxing@hotmail.com>
2015-05-23 17:54:45 +0200

    you can now set the text of the listViewItem

--- MailBowser/src/main/java/edu/chl/mailbowser/contact/views/ContactListViewItem.java
+++ MailBowser/src/main/java/edu/chl/mailbowser/contact/views/ContactListViewItem.java
@@ -2,8 +2,10 @@ package edu.chl.mailbowser.contact.views;
 
 import edu.chl.mailbowser.contact.Contact;
 import edu.chl.mailbowser.contact.IContact;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 
 import java.io.IOException;
@@ -14,6 +16,8 @@ import java.util.ResourceBundle;
  * Created by jesper on 2015-05-22.
  */
 public class ContactListViewItem extends FlowPane{
+    @FXML private Label nameLabel;
+
     private IContact contact;
 
     public ContactListViewItem (IContact contact){
@@ -31,5 +35,9 @@ public class ContactListViewItem extends FlowPane{
 
     public IContact getContact(){
         return contact;
+    }
+
+    public void setText(String text){
+        nameLabel.setText(text);
     }
 }
