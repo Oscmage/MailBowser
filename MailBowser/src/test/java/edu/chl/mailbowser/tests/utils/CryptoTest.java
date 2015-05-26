715fec7ec4ac6e6f84a88c17e45171b30ed34b58
Jaxing <jesper_jaxing@hotmail.com>
2015-05-26 09:08:15 +0200

    Tested edge cases

--- MailBowser/src/test/java/edu/chl/mailbowser/tests/utils/CryptoTest.java
+++ MailBowser/src/test/java/edu/chl/mailbowser/tests/utils/CryptoTest.java
@@ -15,5 +15,25 @@ public class CryptoTest {
         String result = Crypto.decryptByteArray(encrypted, key);
         assertEquals(testText,result);
 
+        boolean exceptionThrown = false;
+        try {
+            encrypted = Crypto.encryptString(testText,"dfghjklölkuytredfvbn");
+        } catch (IllegalArgumentException e) {
+            exceptionThrown = true;
+        }
+        assertTrue(exceptionThrown);
+        exceptionThrown = false;
+        try {
+            result = Crypto.decryptByteArray(new byte[]{1,4,2,64,3,4,2}, key);
+        } catch (Exception e) {
+            exceptionThrown = true;
+        }
+        assertTrue(exceptionThrown);
+        try {
+            Crypto.encryptString(null,key);
+        } catch (Exception e) {
+            exceptionThrown = true;
+        }
+        assertTrue(exceptionThrown);
     }
 }
