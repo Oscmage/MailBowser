de7f1463548870fa0bf93a0f99bf883a863bb9b0
Jaxing <jesper_jaxing@hotmail.com>
2015-05-26 13:09:50 +0200

    Documented email package

--- MailBowser/src/main/java/edu/chl/mailbowser/email/IAddress.java
+++ MailBowser/src/main/java/edu/chl/mailbowser/email/IAddress.java
@@ -10,8 +10,23 @@ import java.io.Serializable;
  * An interface for Address.
  */
 public interface IAddress extends Searchable, Serializable{
+
+    /**
+     * Returns a string representation of the address, in the format user@domain.com
+     *
+     * @return a string representation of the address
+     */
     String toString();
+
+    /**
+     * @return a copy of the javax.mail.InternetAddress object which holds the information
+     */
     javax.mail.Address getJavaMailAddress();
+
+    /**
+     * Returns a string representation of the address, in the format user@domain.com
+     * @return
+     */
     String getString();
     boolean equals(Object o);
     int hashCode();
