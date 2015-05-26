1c95d78715ff06badad3c7fbdf92fa6cffe69230
Mats Högberg <mats@hgbrg.se>
2015-05-26 13:36:16 +0200

    Added missing documentation to the event package

--- MailBowser/src/main/java/edu/chl/mailbowser/event/IObserver.java
+++ MailBowser/src/main/java/edu/chl/mailbowser/event/IObserver.java
@@ -2,10 +2,12 @@ package edu.chl.mailbowser.event;
 
 /**
  * Created by mats on 29/04/15.
+ *
+ * An interface for objects that can listen to observables.
  */
 public interface IObserver {
     /**
-     * This method gets called whenever something happens in an observable that this observer listens to.
+     * This method gets called whenever something happens in an observable that this observer is listening to.
      *
      * @param evt the event that is sent by the observable
      */
