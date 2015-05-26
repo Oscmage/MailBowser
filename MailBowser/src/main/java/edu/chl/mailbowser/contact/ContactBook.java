dc31dae54fb659a21972071fde4def9761d5e1f1
Jaxing <jesper_jaxing@hotmail.com>
2015-05-26 10:13:20 +0200

    Contactbook can write and read contacts

--- MailBowser/src/main/java/edu/chl/mailbowser/contact/ContactBook.java
+++ MailBowser/src/main/java/edu/chl/mailbowser/contact/ContactBook.java
@@ -1,8 +1,10 @@
 package edu.chl.mailbowser.contact;
 
 import edu.chl.mailbowser.event.EventBus;
+import edu.chl.mailbowser.io.*;
 import edu.chl.mailbowser.search.Searcher;
 
+import javax.naming.CompositeName;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -33,4 +35,26 @@ public class ContactBook implements IContactBook{
     public Set<IContact> getMatchingContacts(String query) {
         return Searcher.search(this.contacts,query);
     }
+
+    @Override
+    public boolean readContacts(String filename) {
+        IObjectReader<TreeSet<IContact>> objectReader = new ObjectReader<>();
+
+        try {
+            contacts = objectReader.read(filename);
+            return true;
+        } catch (ObjectReadException e) {
+            //initiate contacts to a new empty TreeSet
+            contacts = new TreeSet<>();
+            return false;
+        }
+    }
+
+    @Override
+    public boolean writeContacts(String filename) {
+        IObjectWriter<TreeSet<IContact>> objectWriter = new ObjectWriter<>();
+        return objectWriter.write((TreeSet<IContact>)contacts, filename); 
+    }
+
+
 }
